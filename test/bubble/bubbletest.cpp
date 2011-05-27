a8a2e59ea4067b12e06983d6f080fbeae896e0ee
Nicole Tietz-Sokolskaya <me@ntietz.com>
2011-05-26 21:42:14 -0400

    Added test for bubblesort, updated comment in mergetest. Updated makefile.

--- test/bubble/bubbletest.cpp
+++ test/bubble/bubbletest.cpp
@@ -1,14 +1,14 @@
-//file:     mergetest.cpp
+//file:     bubbletest.cpp
 //author:   ntietz
 //date:     2011.5.26
 
-#include "../../src/merge/mergesort.h"
+#include "../../src/bubble/bubblesort.h"
 #include <vector>
 #include <iostream>
 
 int main() {
 
-    mergesort<int>* mySort = new mergesort<int>();
+    bubblesort<int>* mySort = new bubblesort<int>();
 
     int arrData[] = {45, 89, 23, 1, 345, -123, 346, 1204, 12040230, 123, 123134, 123, 536457, 123, -12345, -234};
     std::vector<int> data;
