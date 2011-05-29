00a4977cffdc0d09976a8e6372b2eed3afd6157f
Nicole Tietz-Sokolskaya <me@ntietz.com>
2011-05-29 16:31:04 -0400

    Added constructors to the chromosome.

--- src/chromosome/chromosome.cpp
+++ src/chromosome/chromosome.cpp
@@ -3,5 +3,13 @@
 //date:     2011.5.26
 // Implementation of a chromosome
 
+#include "chromosome.hpp"
 
+chromosome::chromosome() {
+    bits = null;
+}
+
+chromosome::chromosome(std::vector<bool> newBits) {
+    bits = new std::vector<bool>(newBits);
+}
 
