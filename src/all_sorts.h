fe32ca75d543e4d17642616ef5aa0707e7486d57
Nicole Tietz-Sokolskaya <me@ntietz.com>
2011-06-15 23:15:32 -0400

    Added include file for all sorting algorithms (so they don't have to be included individually).

---
+++ src/all_sorts.h
@@ -0,0 +1,13 @@
+//file:     all_sorts.h
+//author:   ntietz
+//date:     2011.6.15
+// General include file for all sorting algorithms.
+
+#ifndef _ALL_SORTS_H_
+#define _ALL_SORTS_H_
+
+#include "bubble/bubblesort.h"
+#include "merge/mergesort.h"
+#include "quick/quicksort.h"
+
+#endif
