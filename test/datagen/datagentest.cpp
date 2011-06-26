da6c662bb309157f66274298ef49a376d17506d7
Nicole Tietz-Sokolskaya <me@ntietz.com>
2011-06-26 13:54:37 -0400

    Added datagen test.

---
+++ test/datagen/datagentest.cpp
@@ -0,0 +1,14 @@
+//file:     datagentest.cpp
+//author:   ntietz
+//date:     2011.6.26
+// Tests the datagen class.
+#include "../../src/datagen/datagen.hpp"
+#include <iostream>
+
+int main() {
+
+    DataGenerator gen;
+    std::cout << gen.getUint() << std::endl;
+
+}
+
