3a412fc1dabd4f46d26de0fd34e3d396d7a573a2
Nicholas Tietz <ntietz@gmail.com>
2011-07-16 17:03:57 -0400

    Added a pair class, refactored single point crossover to return a pair instead of an in-place recombination.

---
+++ src/pair/pair.h
@@ -0,0 +1,16 @@
+//file:     pair.h
+//author:   ntietz
+//date:     2011.7.16
+// Implements a generic pair
+
+#ifndef _PAIR_H_
+#define _PAIR_H_
+
+template < typename T >
+class Pair {
+public:
+    T first;
+    T second;
+};
+
+#endif
