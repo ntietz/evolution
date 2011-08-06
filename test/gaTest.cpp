447643d58a032bc4eaa438631ce5fa5299fa2272
Nicholas Tietz <ntietz@gmail.com>
2011-08-06 18:39:58 -0400

    Added to the genetic algorithm test and fixed a bug.

--- test/gaTest.cpp
+++ test/gaTest.cpp
@@ -6,10 +6,62 @@
 #include "gtest/gtest.h"
 #include "ga/ga.h"
 
-TEST(GeneticAlgorithm, GetRandomChromosome) {
-    int size = 20;
-    Chromosome chr = getRandomChromosome(size);
+class Fitness {
+public:
+    int operator()(const Chromosome& candidate) const {
+        int score = 0;
 
-    EXPECT_EQ(size, chr.size());
+        for (int index = 0; index < candidate.size(); ++index) {
+            if (candidate[index] == 0) {
+                ++score;
+            }
+        }
+
+        return score;
+    }
+};
+
+TEST(GeneticAlgorithm, GetRandomChromosome) {
+    // set the size sufficiently large to make the distribution apparent
+    int size = 5000;
+    DataGenerator* rng = new DataGenerator(1024, 1024);
+    Chromosome chr = getRandomChromosome(size, rng);
+
+    ASSERT_EQ(size, chr.size());
+
+    int zeroCount = 0;
+    int onesCount = 0;
+
+    for (int index = 0; index < size; ++index) {
+        if (chr[index] == true) {
+            ++onesCount;
+        } else {
+            ++zeroCount;
+        }
+    }
+
+    EXPECT_LT(zeroCount, (size / 2) + (size / 100));
+    EXPECT_LT(onesCount, (size / 2) + (size / 100));
+}
+
+TEST(GeneticAlgorithm, KTournamentSelection) {
+    Fitness fitness;
+    int populationSize = 100;
+    int childrenPopulationSize = 100;
+    int chromosomeSize = 25;
+    int tournamentSize = 10;
+    DataGenerator* rng = new DataGenerator(1024, 1024);
+
+    KTournamentSelection<Fitness> selection(tournamentSize, childrenPopulationSize, rng, fitness);
+
+    Population population(populationSize);
+    for (int index = 0; index < populationSize; ++index) {
+        population[index] = getRandomChromosome(chromosomeSize);
+    }
+
+    Population children = selection(population);
+
+    ASSERT_EQ(childrenPopulationSize, children.size());
+
 }
 
