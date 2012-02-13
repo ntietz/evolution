46aa15a13f2fc11ac5e31aa7d9c0b8f57fd7c085
Nicole Tietz-Sokolskaya <me@ntietz.com>
2012-02-13 09:32:53 -0500

    Fixed style in chromosome files.

--- src/chromosome/chromosome.cpp
+++ src/chromosome/chromosome.cpp
@@ -5,39 +5,48 @@
 
 #include "chromosome.hpp"
 
-Chromosome::Chromosome() {
+Chromosome::Chromosome()
+{
     // do nothing
 }
 
-Chromosome::Chromosome(std::vector<bool> newBits) {
+Chromosome::Chromosome(std::vector<bool> newBits)
+{
     bits = newBits;
 }
 
-void Chromosome::setBits(std::vector<bool> newBits) {
+void Chromosome::setBits(std::vector<bool> newBits)
+{
     bits = newBits;
 }
 
-std::vector<bool> Chromosome::getBits() const {
+std::vector<bool> Chromosome::getBits() const
+{
     return bits;
 }
 
-void Chromosome::set(int index, bool value) {
+void Chromosome::set(int index, bool value)
+{
     bits[index] = value;
 }
 
-bool Chromosome::get(int index) const {
+bool Chromosome::get(int index) const
+{
     return bits[index];
 }
 
-int Chromosome::size() const {
+int Chromosome::size() const
+{
     return bits.size();
 }
 
-void Chromosome::flip(int index) {
+void Chromosome::flip(int index)
+{
     bits[index] = !bits[index];
 }
 
-std::vector<Chromosome> Chromosome::split(int splitPoint) const {
+std::vector<Chromosome> Chromosome::split(int splitPoint) const
+{
     std::vector<Chromosome> results;
 
     long int leftSize = (bits.size() < splitPoint) ? (bits.size() - 1) : splitPoint;
@@ -46,11 +55,13 @@ std::vector<Chromosome> Chromosome::split(int splitPoint) const {
     std::vector<bool> left;
     std::vector<bool> right;
 
-    for (int index = 0; index < (int)leftSize; ++index) {
+    for (int index = 0; index < (int)leftSize; ++index)
+    {
         left.push_back(bits[index]);
     }
 
-    for (int index = 0; index < (int)rightSize; ++index) {
+    for (int index = 0; index < (int)rightSize; ++index)
+    {
         right.push_back(bits[index + leftSize]);
     }
 
@@ -60,30 +71,42 @@ std::vector<Chromosome> Chromosome::split(int splitPoint) const {
     return results;
 }
 
-Chromosome Chromosome::merge(const Chromosome& left, const Chromosome& right) {
+Chromosome Chromosome::merge( const Chromosome& left
+                            , const Chromosome& right
+                            )
+{
     std::vector<bool> newBits;
 
-    for (int index = 0; index < (int)left.size(); ++index) {
+    for (int index = 0; index < (int)left.size(); ++index)
+    {
         newBits.push_back(left[index]);
     }
 
-    for (int index = 0; index < (int)right.size(); ++index) {
+    for (int index = 0; index < (int)right.size(); ++index)
+    {
         newBits.push_back(right[index]);
     }
 
     return Chromosome(newBits);
 }
 
-bool Chromosome::operator[](int index) const {
+bool Chromosome::operator[](int index) const
+{
     return bits[index];
 }
 
-bool Chromosome::operator==(const Chromosome& other) const {
-    if (bits.size() != other.size()) {
+bool Chromosome::operator==(const Chromosome& other) const
+{
+    if (bits.size() != other.size())
+    {
         return false;
-    } else {
-        for (int index = 0; index < bits.size(); ++index) {
-            if (bits[index] != other[index]) {
+    }
+    else
+    {
+        for (int index = 0; index < bits.size(); ++index)
+        {
+            if (bits[index] != other[index])
+            {
                 return false;
             }
         }
@@ -91,7 +114,8 @@ bool Chromosome::operator==(const Chromosome& other) const {
     }
 }
 
-bool Chromosome::operator!=(const Chromosome& other) const {
+bool Chromosome::operator!=(const Chromosome& other) const
+{
     return !((*this) == other);
 }
 
