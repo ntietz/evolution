f566641a87fc481125a34c12869f7dc63668bda1
Nicholas Tietz <ntietz@gmail.com>
2012-11-25 19:03:39 -0500

    Began major refactoring of the GA code... I commented out almost all of the code, so the tests will NOT compile. I defined the types for the different functions. I set the main body of the GA class to use the new types.

--- lines.sh
+++ lines.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-find . | grep -v "^\./\.git" | grep -v "lib" | grep "\.cpp$\|\.hpp$\|\.h$" | xargs wc -l
+find . | grep -v "^\./\.git" | grep -v "\.swp$" | grep -v "lib\|samples\|test" | grep "\.cpp\|\.hpp\|\.h$" | xargs wc -l
 
